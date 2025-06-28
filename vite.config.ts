eff126fed7a84fc9593489b250e56c704a87b628
AbuAli85 <chairman@falconeyegroup.net>
2025-06-28 15:41:56 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',           // Use jsdom for React tests
-    globals: true,                  // Allow global test API (describe, it, expect)
-    setupFiles: './src/setupTests.ts'
-  },
-  // aliasing is not usually needed for @testing-library/react
-  // but if you need to alias something, do it like this:
-  // resolve: {
-  //   alias: {
-  //     'my-alias': '/actual/path',
-  //   },
-  // },
+  base: '/extra-contracts/', // 👈 Add this line
 })
